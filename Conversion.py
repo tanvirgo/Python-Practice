e423566f3701f4132a4683eafcab6007892842d6
Tanna Henry <tannahenry@Tannas-MacBook-Pro.local>
2025-03-13 14:24:58 -0400

    Conversion

---
+++ Conversion.py
@@ -0,0 +1,7 @@
+# Create a function to convert user input to inches
+
+feet = 12
+x = int(input("Let's see how feet converts to inches!"))
+def feet_to_inches(feet):
+    return feet * 12
+print(feet_to_inches(x))
